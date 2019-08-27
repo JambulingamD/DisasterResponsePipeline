dd9f29c882a32c2d3ab31aad131cd6a3aa566ce7
JambulingamD <32940322+JambulingamD@users.noreply.github.com>
2019-08-26 21:26:24 -0400

    Add files via upload

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,1 +1,84 @@
+import sys
+import pickle
+import nltk
+nltk.download(['punkt', 'wordnet'])
+nltk.download('stopwords')
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import pandas as pd
+from sqlalchemy import create_engine
+from nltk.corpus import stopwords
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
+def load_data(database_filepath):
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_query('select * from cleanData', engine)
+    X = df['message'].values
+    Y = df.drop(['id','message','original','genre'], axis=1)
+    category_names = Y.columns
+    return X, Y, category_names
+
+def tokenize(text):
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for token in tokens:
+        clean_token = lemmatizer.lemmatize(token).lower().strip()
+        clean_tokens.append(clean_token)
+    return clean_tokens
+
+def build_model():
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    parameters = {'vect__ngram_range': ((1, 1), (1, 2)),'clf__estimator__min_samples_split': [2, 4],}
+    cv = GridSearchCV(pipeline, param_grid=parameters, verbose=2, n_jobs=4)
+    return cv
+ 
+def evaluate_model(model, X_test, Y_test, category_names):
+    Y_pred = model.predict(X_test)
+    print(classification_report(Y_test, Y_pred, target_names=category_names))
+    print("Accuracy scores per category\n")
+    for i in range(25):
+        print("Accuracy score for " + Y_test.columns[i], accuracy_score(Y_test.values[:,i],Y_pred[:,i]))
+
+def save_model(model, model_filepath):
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
